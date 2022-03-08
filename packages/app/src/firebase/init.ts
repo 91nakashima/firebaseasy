35dd493ad49d3363e522367adabfe16075c7a239
91nakashima <91nakashima@gmail.com>
2022-03-08 12:55:23 +0900

    修正

--- packages/app/src/firebase/init.ts
+++ packages/app/src/firebase/init.ts
@@ -1,14 +1,6 @@
 import { initializeApp } from 'firebase/app'
 
-const config = {
-  apiKey: 'AIzaSyDLqCaS5enYj11Jata5WC2TpGXx_Y3xSXE',
-  authDomain: 'yapoyapo.com',
-  projectId: 'yapo-b3e1d',
-  storageBucket: 'yapo-b3e1d.appspot.com',
-  messagingSenderId: '553598338231',
-  appId: '1:553598338231:web:3ad71813c648801019f932',
-  measurementId: 'G-2DBWHNEZ5B'
-}
+const config = {}
 
 const app = initializeApp(config)
 
